d039ac16efd62d7275ab9f738b0d516b37041d07
LucaasAndrade <lucasmarcelodeandradesilva@gmail.com>
2022-07-16 17:38:17 -0300

    Exercicío 17 - OK

--- 4. ARRAY/exercicio17.cs
+++ 4. ARRAY/exercicio17.cs
@@ -2,4 +2,32 @@
 // Ex: [4,2,1,3,7] => [1,2,4,3,7]
 
 
-// TESTE
+void PrintarArray(int[] array)
+{
+    foreach(int i in array)
+    {
+        Console.WriteLine($"{i} ");
+    }
+        Console.WriteLine("--------");
+}
+
+int[] OrganizarArray(int[] array)
+{
+    int atual = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        for(int j = 0; j < array.Length; j++)
+        {
+            if(array[j] > array[i])
+            {
+                atual = array[j];
+                array[j] = array[i];
+                array[i] = atual;
+            }
+        }
+    }
+    return array;
+}
+
+int[] x = {4,2,1,3,7};
+PrintarArray(OrganizarArray(x));
--- 4. ARRAY/exercicio18.cs
+++ 4. ARRAY/exercicio18.cs
@@ -1,2 +1,5 @@
 // 18. Implementar uma função que recebendo um array de números inteiros, retorne seu maior produto.
 // Ex: [4,2,1,3,2] => 12
+
+
+
